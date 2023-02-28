578792776f6941330502b2438b6364818a7aa957
huongle158 <lehuongvt15@gmail.com>
2023-02-28 15:38:08 +0700

    cors

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://localhost:3000/', // Chỉ cho phép domain example.com gửi yêu cầu
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Cho phép sử dụng các phương thức HTTP
-    allowedHeaders: 'Content-Type, Accept', // Cho phép sử dụng các header được chỉ định
-    credentials: true, // Cho phép sử dụng cookies và headers chứa thông tin xác thực
-  }); // Kích hoạt middleware CORS ở đây
+  const app = await NestFactory.create(AppModule, { cors: true });
+
   await app.listen(3000);
 }
 bootstrap();
